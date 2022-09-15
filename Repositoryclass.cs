1fe22d22a34d3ff5616cd3fbe12f443081786671
Shaswat Misra <shaswatmisra2310@gmail.com>
2022-09-15 18:29:00 +0530

    Add project files.

---
+++ Repositoryclass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using StockTrackerApp.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+//using ConsoleApp_DB.Models;
+namespace BatchProcessorConsole
+{
+    public class Repositoryclass
+    {
+        //StockTrackerContext context = new StockTrackerContext();
+
+        //public Repostoryclass(StockTrackerContext _context)
+        //{
+        //    context = _context;
+        //}
+
+        //public Header GetHeader()
+        //{
+        //    Header header = context.HeaderTable.FirstOrDefault<Header>(p => p.Key == "ed8bbc525fmshe7f2ca46e69c3ffp179016jsne9ad147cd3e0");
+        //    return header;
+        //}
+    }
+}
