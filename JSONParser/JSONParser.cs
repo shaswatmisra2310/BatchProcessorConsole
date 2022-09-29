b0ebedd2ff88fc2ce8460a14c859c88da28b7122
zyucc <110530125+zeissyusuf@users.noreply.github.com>
2022-09-29 13:22:39 +0530

    Update JSONParser.cs

--- JSONParser/JSONParser.cs
+++ JSONParser/JSONParser.cs
@@ -18,6 +18,19 @@ namespace JSONParser
             var symbol = data.SelectToken("quoteResponse.result[0].symbol")?.Value<string>();
             var exchange = data.SelectToken("quoteResponse.result[0].exchange")?.Value<string>();
             var region = data.SelectToken("quoteResponse.result[0].region")?.Value<string>();
+            var longName = data.SelectToken("quoteResponse.result[0].longName")?.Value<string>();
+            var totalCash = data.SelectToken("quoteResponse.result[0].totalCash")?.Value<string>();
+            var preMarketPrice = data.SelectToken("quoteResponse.result[0].preMarketPrice")?.Value<string>();
+            var regularMarketPrice = data.SelectToken("quoteResponse.result[0].regularMarketPrice")?.Value<string>();
+            try
+            {
+                var Industry = data.SelectToken("quoteResponse.result[0].Industry")?.Value<string>();
+            }
+
+            catch(InvalidCastException e)
+            {
+                var Industry = "null";
+            }
 
             Quotes q = new Quotes() { SymbolName=symbol, QuoteType=quoteType,Name=quoteSourceName, Exchange =exchange, Industry="Industry name"};
 
